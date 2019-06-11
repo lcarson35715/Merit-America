03ceb704eaee374a1ede94d201d9993ffcee0768
lcarson35715 <50433348+lcarson35715@users.noreply.github.com>
2019-06-11 10:29:33 -0500

    Add files via upload

---
+++ FizzBuzz.js
@@ -0,0 +1,19 @@
+public class FizzBuzzTest {
+    public String fizzBuzz(int number) {
+        if(int number % 3 === 0){
+        	 System.out.println(Fizz);
+        }
+        if(int number % 5 === 0){
+        	 System.out.println(Buzz);
+        
+        }
+        if(int number % 15 === 0){
+        	 System.out.println(FizzBuzz);
+        }
+
+        else{
+        	if(int number % 15 === 0){
+        	 System.out.println(int number);
+        }
+    }
+}
