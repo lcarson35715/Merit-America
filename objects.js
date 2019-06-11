28a5da01d6c8afbcaacc9076922d08ca95fa5647
lcarson35715 <50433348+lcarson35715@users.noreply.github.com>
2019-06-10 20:09:42 -0500

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,13 @@
+var users = [
+{name: "Michael", age:37},
+{name: "John", age:30},
+{name: "David", age:27}
+];
+
+console.log(users[1].age); //Johns Age
+console.log(users[0].name); //Name of first object
+
+
+for(var i = 0; i < arr.length; i++){
+	console.log(users[i].name users[i].age);
+}
